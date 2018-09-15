9fbff41f4a28a16463e67e7ba93b7e52d60cc676
dudueasy <dudueasy@126.com>
2018-09-16 02:10:09 +0800

    完成 Material-UI 的服务端渲染初始化配置

--- client/server-entry.js
+++ client/server-entry.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import {StaticRouter} from 'react-router-dom'
 import {Provider, useStaticRendering} from 'mobx-react'
+import JssProvider from 'react-jss/lib/JssProvider';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 
 import {createStoreMap} from './store/store'
 import App from './views/App'
@@ -9,14 +11,27 @@ import App from './views/App'
 useStaticRendering(true)
 require('source-map-support').install()
 
+
 // server-bundle 是一个函数 可以接收 store 作为 provider 传递的数据
-export default (stores, routerContext, url) => (
+export default function createApp(stores, routerContext, url, MUIConfig = {}) {
+  const {
+    sheetsRegistry, generateClassName, theme, sheetsManager,
+  } = MUIConfig
+
   // stores = {store: xxx, ...}
-  <Provider {...stores}>
-    <StaticRouter context={routerContext} location={url}>
-      <App/>
-    </StaticRouter>
-  </Provider>
-)
+  return (
+    <Provider {...stores}>
+      <StaticRouter context={routerContext} location={url}>
+
+        <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
+          <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
+            <App/>
+
+          </MuiThemeProvider>
+        </JssProvider>
+      </StaticRouter>
+    </Provider>
+  )
+}
 
 export {createStoreMap}
--- util/server-render.js
+++ util/server-render.js
@@ -4,6 +4,29 @@ const ejs = require('ejs')
 const serialize = require('serialize-javascript')
 
 
+// MUI dependencies and MUI-SSR config
+const {SheetsRegistry} = require('react-jss/lib/jss')
+const {createGenerateClassName, createMuiTheme} = require('@material-ui/core/styles')
+const pink = require('@material-ui/core/colors/pink').default
+const lightBlue = require('@material-ui/core/colors/lightBlue').default
+
+
+// generate MUI variables, create MUIConfig
+const sheetsRegistry = new SheetsRegistry();
+const generateClassName = createGenerateClassName();
+const sheetsManager = new Map();
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    accent: lightBlue,
+    type: 'light',
+  },
+});
+
+
+let MUIConfig = {sheetsRegistry, generateClassName, theme, sheetsManager}
+
+
 // getStoreStete 的返回值: {appState: {count: xx, name: xx}}
 const getStoreState = (stores) => {
   return Object.keys(stores).reduce((result, item) => {
@@ -11,8 +34,9 @@ const getStoreState = (stores) => {
   }, {})
 }
 
-module.exports = (bundle, template, req, res)=>{
-  return new Promise((resolve,reject)=>{
+
+module.exports = function serverRender(bundle, template, req, res) {
+  return new Promise((resolve, reject) => {
 
     const createStoreMap = bundle.createStoreMap
     const createApp = bundle.default
@@ -24,7 +48,7 @@ module.exports = (bundle, template, req, res)=>{
 
 
     // serverBundle( store, context_for_staticRouter, url_for_staticRouter)
-    const app = createApp(stores, routerContext, req.url)
+    const app = createApp(stores, routerContext, req.url, MUIConfig)
 
     asyncBoostrap(app).then(() => {
 
@@ -45,7 +69,7 @@ module.exports = (bundle, template, req, res)=>{
 
 
       // 设置 <header> SEO
-      const { Helmet }= require('react-helmet')
+      const {Helmet} = require('react-helmet')
       const helmet = Helmet.renderStatic();
 
       // 向前端页面插入 组件, state 数据以及 helmet
@@ -63,3 +87,23 @@ module.exports = (bundle, template, req, res)=>{
   })
 
 }
+
+
+// Create a sheetsRegistry instance.
+// const sheetsRegistry = new SheetsRegistry();
+
+// Create a sheetsManager instance.
+// const sheetsManager = new Map();
+
+// Create a theme instance.
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: green,
+//     accent: red,
+//     type: 'light',
+//   },
+// });
+
+// Create a new class name generator.
+// const generateClassName = createGenerateClassName();
+
