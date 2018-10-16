bb6d59764506add8f3144eb476efad5a008f6918
dudueasy <dudueasy@126.com>
2018-10-17 00:35:07 +0800

    初步完成话题详情页面

--- client/views/topic-detail/index.js
+++ client/views/topic-detail/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import PropTypes from 'prop-types'
 import marked from 'marked'
 import {Helmet} from 'react-helmet'
@@ -11,22 +11,75 @@ import Container from '../layout/container'
 // import {topicDetailStyle} from './styles'
 // import Reply from './reply'
 
+const style = theme => ({
+  '@global': {
+    html: {
+      height: '100%'
+    }
+  },
+  content: {
+    minHeight: '100%'
+  }
+})
 
+@withStyles(style)
 @inject(stores => ({topicStore: stores.topicStore}))
+@observer
 export default class TopicDetail extends React.Component {
-  // constructor() {
-  // }
+  constructor(props) {
+    super(props)
+    this.props.topicStore.updateTopicTab('')
+    this.props.topicStore.fetchTopicDetail(this.getTopicId)
+  }
 
   componentDidMount() {
     // do something here
   }
 
+  get getTopicId() {
+    return this.props.match.params.id
+  }
+
   render() {
-    const topic = this.props.topicStore.topicDataList[0]
-    return (<div>This is topic detail component</div>)
+
+    const topicId = this.getTopicId
+    console.log('topicId:', topicId)
+
+
+    const {
+      classes,
+      topicStore: {
+        topicDetail, topicDetail: {content}
+      }
+    } = this.props
+
+
+    return (
+      <Fragment>
+
+        <Helmet>
+          <title>This is topic detail page</title>
+          <meta name="description" content="this is description"/>
+        </Helmet>
+
+        <div className={classes.content}>
+          {content
+            ? <div dangerouslySetInnerHTML={{__html: marked(content)}}/>
+            : null
+          }
+        </div>
+      </Fragment>
+    )
   }
 }
 
 TopicDetail.wrappedComponent.propTypes = {
   topicStore: PropTypes.object.isRequired
+}
+
+
+TopicDetail.propTypes = {
+  classes: PropTypes.object.isRequired, //eslint-disable-line
+  location: PropTypes.object.isRequired, //eslint-disable-line
+  match: PropTypes.object.isRequired,//eslint-disable-line
 }
