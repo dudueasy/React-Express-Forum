21cc90deb29bacb2b223a1fcd5c9af0d8c574fde
dudueasy <dudueasy@126.com>
2019-03-05 11:36:11 +0800

    修正客户端渲染时的打包配置 - 不再打包服务端 bundle

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,15 +1,15 @@
-const path = require('path')
-const webpack = require('webpack')
-const webpackMerge = require('webpack-merge')
-const HTMLPlugin = require('html-webpack-plugin')
-const baseConfig = require('./webpack.config.base')
+const path = require('path');
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const HTMLPlugin = require('html-webpack-plugin');
+const baseConfig = require('./webpack.config.base');
 
-const isDev = process.env.NODE_ENV === 'development'
-require('dotenv').config()
+const isDev = process.env.NODE_ENV === 'development';
+require('dotenv').config();
 
 let config = webpackMerge(baseConfig, {
   entry: {
-    app: path.join(__dirname, '../client/app.js')
+    app: path.join(__dirname, '../client/app.js'),
   },
   output: {
     filename: '[name].[hash].js',
@@ -18,42 +18,38 @@ let config = webpackMerge(baseConfig, {
   },
   plugins: [
     new HTMLPlugin({template: path.join(__dirname, '../client/template.html')}),
-    new HTMLPlugin({
-      template: '!!ejs-compiled-loader!' + path.join(__dirname, '../client/server.template.ejs'),
-      filename: 'server.ejs'
-    }),
     new webpack.DefinePlugin({
-      'process.env.API_BASE': JSON.stringify(`${process.env.HOST_NAME}:${process.env.NODE_SERVER_PORT}`)
+      'process.env.API_BASE': JSON.stringify(`${process.env.HOST_NAME}:${process.env.NODE_SERVER_PORT}`),
     }),
-  ]
-})
+  ],
+});
 
 if (isDev) {
-  console.log('current env is development')
-  config.devtool = "#cheap-module-eval-source-map"
+  console.log('current env is development');
+  config.devtool = "#cheap-module-eval-source-map";
   config.entry = {
     app: [
       'react-hot-loader/patch',
-      path.join(__dirname, '../client/app.js')
-    ]
-  }
+      path.join(__dirname, '../client/app.js'),
+    ],
+  };
   config.devServer = {
     host: '0.0.0.0',
     port: process.env.DEV_SERVER_PORT,
     contentBase: path.join(__dirname, '../dist'),
     hot: true,
     overlay: {
-      errors: true
+      errors: true,
     },
     publicPath: '/public/',
     historyApiFallback: {
-      index: '/public/index.html'
+      index: '/public/index.html',
     },
     proxy: {
-      '/api': `http://localhost:${process.env.NODE_SERVER_PORT}`
-    }
-  }
-  config.plugins.push(new webpack.HotModuleReplacementPlugin())
+      '/api': `http://localhost:${process.env.NODE_SERVER_PORT}`,
+    },
+  };
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
-module.exports = config
+module.exports = config;
